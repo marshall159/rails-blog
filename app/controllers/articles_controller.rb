ed1396bc89a6fdfc71ead3cd62f6c1bb9792b5f3
Aneel Marshall <aneel.marshall@outlook.com>
2018-09-28 15:50:16 +0100

    feat: add article index action and view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,24 @@
 class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
   end
 
   def create
     @article = Article.new(article_params)
 
-    saved = @article.save
+    @article.save
     redirect_to @article
   end
+
+
 
   private
     def article_params
